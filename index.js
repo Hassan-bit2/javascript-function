8bed1236562a85541fa64182e61e01cf50a04489
Taha <Tahahassanbukhari@gmail.com>
2022-05-06 21:30:49 +0500

    javascript onclick function

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function greet() {
-    document.write("Greetings panda!");
+    alert("GREETINGS PANDA!");
+}
+
+function info(name, age) {
+    document.write("Name is " + name + " age is " + age);
+}
+
+function refresh() {
+    document.write("Please refresh the page!");
+}
+
+function mOver() {
+    document.write("I am out!");
+}
+
+function Wind() {
+    document.write("Lets wind the clock back a year!");
 }
